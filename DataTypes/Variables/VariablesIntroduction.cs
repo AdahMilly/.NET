1de250d8de121372f9be1ae6efcda9b34fa9ba05
AdahMilly <oamildred@gmail.com>
2023-11-24 15:10:34 +0300

    variables intro

---
+++ DataTypes/Variables/VariablesIntroduction.cs
@@ -0,0 +1,31 @@
+﻿// we cannont override the value
+//we cannot re-declare
+//we must give an initial value - you cant use var/you can't use the variable before you assign it
+
+int age;
+age = 12;
+
+//we cannot override the value
+//we cannot re-declare
+//we must give an initial value
+const int title = 2008;
+
+//how to declare a variable
+//start with the data type, then the variable name, lastly for mutable is assigning a value.
+int weight  = 90;
+double height = 151.2;
+float weight2 = 1.0f;
+string name = null;
+
+//another syntax-infer based on the value(using the var keyword)
+//using the var based on the value
+var y = true;
+
+//must start with a letter, underscore
+//can contain characters, decimal digit characters, unicode connecting characters
+//identifier cannto be a reserved keyword
+//you can declare ifentifiers that much C# keywords by using the @ prefix on the identifier. @ is not part of the identifier name.
+//always use meaningful names
+
+//naming convections
+//camelCase, Pascal Case, Hungarian Notation.
