8c1657c091cf344c5b78f86b599fc059b6ca6876
AdahMilly <oamildred@gmail.com>
2023-11-27 15:17:34 +0300

    list, control flow

---
+++ DataTypes/List/ListPp.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes.List
+{
+    public class ListPp
+    {
+        public void ListPractice()
+        {
+            //initializing
+            List<string> teams = new List<string>();
+            //adding
+            teams.Add("Machester United");
+            teams.Add("Real Madrid");
+            teams.Add("Chelsea");
+            teams.Add("Barcelona");
+            teams.Add("Arsenal");
+
+
+            //accessing- indexing
+
+            Console.WriteLine(teams[1]);
+
+            //for Each
+            //foreach (string team in teams) 
+            //{
+            //    Console.WriteLine(team);
+            //}
+
+            //for Loop -indexes
+
+            //for(int i =0 ; i < teams.Count; i++)
+            //{
+            //    Console.WriteLine(teams[i]);
+            //}
+
+            //insert
+            teams.Insert(0, "Brighton");
+
+
+            List<string> germanyteams = new List<string>() {
+                "Bayern Munich",
+                "Stuggard"
+            };
+            teams.InsertRange(0, germanyteams);
+            //for (int i = 0; i < teams.Count; i++)
+            //{
+            //    Console.WriteLine(teams[i]);
+            //}
+            //Contains -available
+            Console.WriteLine(teams.Contains("Machester United"));
+            Console.WriteLine(teams.Contains("Liverpool"));
+            //Remove
+
+            //teams.Remove("Arsenal");
+
+
+            //teams.RemoveAt(teams.Count - 1);
+            //for (int i = 0; i < teams.Count; i++)
+            //{
+            //    Console.WriteLine(teams[i]);
+            //}
+
+            //teams.Clear();
+            //teams.RemoveRange(0, 2);
+
+
+            //teams.RemoveAll(x => x.Equals("Brighton"));
+            teams.Sort();
+            for (int i = 0; i < teams.Count; i++)
+            {
+                Console.WriteLine(teams[i]);
+            }
+
+
+
+        }
+    }
+}
---
+++ DataTypes/List/SortingList.cs
@@ -0,0 +1,20 @@
+﻿namespace lists.ListExamples
+{
+    internal class SortingClass : IComparer<Employee>
+    {
+        public int Compare(Employee? x, Employee? y)
+        {
+            return x.Name.CompareTo(y.Name);
+        }
+
+        public int Compare(Employee? x, Employee? y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void sayHello()
+        {
+            Console.WriteLine("Hello There");
+        }
+    }
+}
