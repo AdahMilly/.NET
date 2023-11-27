a2a47ed5c0437251c804cc5ec7a225763a979753
AdahMilly <oamildred@gmail.com>
2023-11-27 14:10:17 +0300

    arraylist

--- DataTypes/ArrayList/ArrayList.cs
+++ DataTypes/ArrayList/ArrayList.cs
@@ -1,8 +1,70 @@
 ﻿using System.Collections;
+//a non generic type
 
-namespace DataTypes.ArrayList
+
+ArrayList arrayList = new ArrayList()
 {
-    public class ArrayList
-    {
-    }
+   "Meg",
+    29,
+    true,
+    null
+};//declare and initialize items
+
+
+ArrayList arrayList1 = new ArrayList();
+
+//adding items
+arrayList1.Add(1);//int =>object
+arrayList1.Add(2);
+arrayList1.Add(3);
+arrayList1.Add(4);
+arrayList1.Add(4);
+arrayList1.Add("Hello");
+arrayList1.Add(true);
+
+
+//Access by index
+var value2 = (int)arrayList1[2];
+Console.WriteLine($"integer{value2}");
+
+//Foreach- has value
+
+foreach (var item in arrayList1)
+{
+    Console.WriteLine(item);
 }
+
+//Forloop-- has index/values
+
+for (int i = 0; i < arrayList1.Count; i++)
+{
+    Console.WriteLine($"{i}= {arrayList1[i]}");
+}
+
+// Insert 
+arrayList1.Insert(2, 1000);
+arrayList1.InsertRange(1, arrayList);
+
+//for (int i = 0; i < arrayList1.Count; i++)
+//{
+//    Console.WriteLine($"{i}= {arrayList1[i]}");
+//}
+
+//removing 
+//Remove- Single item (dont know index , just know the item)
+//RemoveAt-remove based on index
+//removeRange- several items
+
+arrayList1.Remove("Meg");
+arrayList1.RemoveAt(1);
+arrayList1.RemoveRange(0, 3);
+
+
+
+for (int i = 0; i < arrayList1.Count; i++)
+{
+    Console.WriteLine($"{i}= {arrayList1[i]}");
+}
+
+Console.WriteLine(arrayList1.Contains("Maxon"));
+//Array List, HashTable, Stack, SortedList, Queue are not type safe
