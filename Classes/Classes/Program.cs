e20e5a23d393915ef37c89f6fea7d9b40ef18fe3
AdahMilly <oamildred@gmail.com>
2023-11-28 15:36:02 +0300

    classes

---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Classes;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // class = A bundle of related code.
+            // Can be used as a blueprint to create objects (OOP)
+
+            ClassIntroduction.Hello();
+            ClassIntroduction.Waiting();
+            ClassIntroduction.Bye();
+
+            Console.ReadKey();
+        }
+    }
+}
